5df9477bd372fb697c996271a4c1fc5a5e5f0d7f
Lúcio Magno <devalmagno@gmail.com>
2024-04-17 10:33:28 -0300

    App Page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,9 +1,11 @@
 "use client"
+import { poppins600 } from "@/app/fonts";
 import Loading from "@/components/Loading";
 import { getProjectById } from "@/utils/getProjectById";
 import { IProject } from "@/utils/getProjects";
 import { useEffect, useState } from "react";
 import { FaAngleLeft, FaAngleRight, FaAngleUp, FaAngleDown } from "react-icons/fa";
+import { FaApple, FaAndroid, FaChrome, FaMobile, FaCode } from "react-icons/fa";
 
 export default function Project({ params }: { params: { id: string } }) {
   const [project, setProject] = useState<IProject | null>(null);
@@ -36,37 +38,51 @@ export default function Project({ params }: { params: { id: string } }) {
 
   return (
     <div className="w-full py-8 px-4">
-      <div className="bg-dark-300 rounded-xl flex flex-col overflow-hidden">
-        <div className="w-full m-auto relative group flex justify-center" style={imageExtended ? {} : { height: 320 }}>
+      <div className="bg-dark-300 rounded-xl flex flex-col overflow-hidden shadow-md">
+        <div className={`w-full m-auto relative group flex justify-center sm:max-h-[640px] sm:h-auto ${imageExtended ? "" : "max-h-80 overflow-hidden"}`}>
           <img src={project.images[currentIndex]}
-            className={`w-full h-full bg-center bg-cover duration-500 ${imageExtended ? 'object-contain' : 'object-cover'} sm:object-contain`}
+            className={`z-10 bg-center bg-cover duration-500 ${imageExtended ? 'object-contain' : 'object-cover'} sm:object-contain`}
           />
-          {imageExtended && (
-            <>
-              <div
-                onClick={handleRight}
-                className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
-              >
-                <FaAngleRight />
-              </div>
-              <div
-                onClick={handleLeft}
-                className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
-              >
-                <FaAngleLeft />
-              </div>
-            </>
-          )}
+          <>
+            <div
+              onClick={handleRight} className={`hidden sm:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ${imageExtended ? "group-hover:block" : ""}`} > <FaAngleRight /> </div>
+            <div
+              onClick={handleLeft}
+              className={`hidden sm:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer ${imageExtended ? "group-hover:block" : ""}`}
+            >
+              <FaAngleLeft />
+            </div>
+          </>
           <div
             onClick={() => setImageExtended(state => !state)}
-            className="absolute top-[90%] -translate-x-0 translate-y-[-50%] text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer sm:hidden"
+            className={`absolute top-[90%] -translate-x-0 translate-y-[-50%] text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer sm:hidden ${imageExtended ? "top-10" : ""}`}
           >
             {imageExtended ? <FaAngleUp /> : <FaAngleDown />}
           </div>
         </div>
-        <div className="text-white">
-          <h1>{project.title}</h1>
-          <p>{project.description}</p>
+        <div className="text-white px-8 py-4 flex flex-col">
+          <h1 className={`text-xl ${poppins600.className}`}>{project.title}</h1>
+          <a href={project.source} target="_blank" className="flex gap-2 items-center text-indigo-600">
+            <FaCode /> Code available here
+          </a>
+          <strong>About</strong>
+          <p className="text-title-100">{project.description}</p>
+          <strong>Skills used</strong>
+          <div className='flex gap-2 flex-wrap'>
+            {project.skillsUsed.map(e => (
+              <div className='bg-gray-400 w-max px-2 py-1 text-xs rounded-md shadow-md text-gray-100'>
+                {e}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className='bg-dark-400 py-5 flex items-center justify-center gap-6 text-xl text-icon-100'>
+          {project.platforms.map(e => {
+            if (e === "Android") return <FaAndroid />;
+            if (e === "Ios") return <FaApple />;
+            if (e === "Web") return <FaChrome />;
+            if (e === "Mobile") return <FaMobile />;
+          })}
         </div>
       </div>
     </div>
